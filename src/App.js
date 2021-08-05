5abbcce7c9c155f66261ab553df48436308f18be
Sabjeslav <s4tenko@gmail.com>
2021-08-05 14:37:56 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.sass';
+import AuthForm from './components/AuthForm';
+
+function App () {
+  return (
+    <div className='light-theme'>
+      <AuthForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/validationSchemas.js
@@ -0,0 +1,18 @@
+import * as Yup from 'yup';
+
+export const SignupSchema = Yup.object().shape({
+  login: Yup.string()
+    .min(2, 'Too short!')
+    .max(30, 'Too Long!')
+    .required('Enter login'),
+  password: Yup.string()
+    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$/gm, {
+      message: 'Password must contain at least 1 capital letter and 1 number',
+      excludeEmptyString: true,
+    })
+    .min(6, 'Password must be longer than 6 symbols')
+    .required('Enter password'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Enter email'),
+});
