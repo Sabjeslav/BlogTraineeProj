ca025f511a2efbc362836b265f242a2615fd6d74
Sabjeslav <s4tenko@gmail.com>
2021-08-05 16:16:31 +0300

    Added header routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.sass';
 import AuthForm from './components/AuthForm';
 import Header from './components/Header';
+import Users from './components/Users';
 
 function App () {
   return (
@@ -11,10 +12,10 @@ function App () {
         <Header />
         <Switch>
           <Route exact path='/'>
-            <Redirect to='/login' />
+            <Redirect to='/login' component={AuthForm} />
           </Route>
           <Route path='/login' component={AuthForm} />
-          <Route path='/users' component={AuthForm} />
+          <Route path='/users' component={Users} />
         </Switch>
       </BrowserRouter>
     </div>
