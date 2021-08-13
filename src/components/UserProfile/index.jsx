e0a6f2af2d5c3228921a3e7fef403699d379ddd9
Sabjeslav <s4tenko@gmail.com>
2021-08-13 11:58:26 +0300

    Added image scaling for user profile

--- src/components/UserProfile/index.jsx
+++ src/components/UserProfile/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import style from './UserProfile.module.sass';
 import { connect } from 'react-redux';
 import imgPlaceholder from '../../img/avatar-placeholder.png';
+import cx from 'classnames';
 
 function UserProfile (props) {
   let { user, users } = props;
@@ -10,7 +11,12 @@ function UserProfile (props) {
   if (id) {
     user = users.find(u => u._id === id);
   }
-  console.log(user);  const imgLink = `https://nodejs-test-api-blog.herokuapp.com/${user.avatar}`;
+  const [isScaled, setIsScaled] = useState(false);
+  const scaleImage = () => {
+    setIsScaled(!isScaled);
+  };
+  console.log(user);
+  const imgLink = `https://nodejs-test-api-blog.herokuapp.com/${user.avatar}`;
   return (
     <div className={style.profileWrapper}>
       <div className={style.profileHeader}>Profile</div>
@@ -33,7 +39,14 @@ function UserProfile (props) {
         </div>
         <div className={style.profileData}>
           <div className={style.imgContainer}>
-            <img src={imgLink} alt='' />
+            <img
+              className={cx(style.profileImg, {
+                [style.scaled]: isScaled,
+              })}
+              onClick={scaleImage}
+              src={imgLink}
+              alt=''
+            />
             <img className={style.imgPlaceholder} src={imgPlaceholder} alt='' />
           </div>
         </div>
