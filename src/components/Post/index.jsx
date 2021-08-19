f4ebe516933bd738231a0f708ca87cc747426ecb
Sabjeslav <s4tenko@gmail.com>
2021-08-19 12:05:11 +0300

    Changed post card design

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import React from "react";
 import style from "./Post.module.sass";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
@@ -11,26 +10,24 @@ import Typography from "@material-ui/core/Typography";
 export default function Post({ post }) {
   return (
     <Card className={style.postWrapper}>
-      <CardActionArea>
-        <CardContent>
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="h2"
-            className={style.postText}
-          >
-            {post.title}
-          </Typography>
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            component="p"
-            className={style.postText}
-          >
-            {post.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="h2"
+          className={style.postText}
+        >
+          {post.title}
+        </Typography>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component="p"
+          className={style.postText}
+        >
+          {post.description}
+        </Typography>
+      </CardContent>
       <CardActions>
         <Link className={style.detailsBtn} to={`/posts/${post._id}`}>
           <Button size="small" color="primary">
