905f0fc5ec5e9a0263bb1b724d46dc4930033185
Sabjeslav <s4tenko@gmail.com>
2021-08-19 16:56:02 +0300

    Added modal window warning dialog on account delete

--- src/pages/CurrentUserProfilePage/index.jsx
+++ src/pages/CurrentUserProfilePage/index.jsx
@@ -1,9 +1,17 @@
-import React from "react";
-import style from "./CurrentUserProfilePage.module.sass";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import cx from "classnames";
-import Profile from "../../components/Profile";
 import { useDispatch, useSelector } from "react-redux";
+import cx from "classnames";
+
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+import style from "./CurrentUserProfilePage.module.sass";
+import Profile from "../../components/Profile";
 import {
   deleteCurrentUser,
   logOut,
@@ -16,10 +24,14 @@ function CurrentUserProfilePage() {
   const history = useHistory();
   const { user } = useSelector((state) => state.user);
   const isEditing = useSelector((state) => state.user.isEditing);
+  const [isOpened, setIsOpened] = useState(false);
   const logOutHandler = () => {
     localStorage.clear();
     dispatch(logOut());
     history.push("/signIn");
+  };
+  const toggleModal = () => {
+    setIsOpened(!isOpened);
   };
   const deleteAccount = async () => {
     dispatch(deleteCurrentUser(user._id));
@@ -29,32 +41,58 @@ function CurrentUserProfilePage() {
     dispatch(toggleIsEditing());
   };
   return (
-    <div className={style.profileOuterWrapper}>
-      {isEditing ? (
-        <EditProfileForm editHandler={editHandler} />
-      ) : (
-        <>
-          <Profile user={user} />
-          <div className={cx(style.profileRow, style.buttonRow)}>
-            <button className={style.profileBtn} onClick={editHandler}>
-              Edit profile
-            </button>
-            <button
-              className={cx(style.profileBtn, style.warningBtn)}
-              onClick={deleteAccount}
-            >
-              Delete account
-            </button>
-            <button
-              className={cx(style.profileBtn, style.warningBtn)}
-              onClick={logOutHandler}
-            >
-              Log out
-            </button>
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      <Dialog
+        open={isOpened}
+        onClose={toggleModal}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"This action can not be undone"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            You will not be able to restore your account after this action. Are
+            you sure about that?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={toggleModal} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={deleteAccount} color="secondary" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <div className={style.profileOuterWrapper}>
+        {isEditing ? (
+          <EditProfileForm editHandler={editHandler} />
+        ) : (
+          <>
+            <Profile user={user} />
+            <div className={cx(style.profileRow, style.buttonRow)}>
+              <button className={style.profileBtn} onClick={editHandler}>
+                Edit profile
+              </button>
+              <button
+                className={cx(style.profileBtn, style.warningBtn)}
+                onClick={toggleModal}
+              >
+                Delete account
+              </button>
+              <button
+                className={cx(style.profileBtn, style.warningBtn)}
+                onClick={logOutHandler}
+              >
+                Log out
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
